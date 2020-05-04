f0341cbb9caabd3166f9be85b640e5b16006416b
vince <vincentdev2112@gmail.com>
2020-05-04 11:31:16 -0400

    ressources updates

--- app/src/main/java/com/pam/travail5/persistence/ChatDatabase.java
+++ app/src/main/java/com/pam/travail5/persistence/ChatDatabase.java
@@ -8,7 +8,7 @@ import com.pam.travail5.model.Message;
 import com.pam.travail5.model.Session;
 import com.pam.travail5.model.User;
 
-@Database(entities = {Message.class, Avatar.class, Session.class, User.class}, version = 5)
+@Database(entities = {Message.class, Avatar.class, Session.class, User.class}, version = 6)
 public abstract class ChatDatabase extends RoomDatabase {
     public abstract MessageDao getMessageDao();
 
